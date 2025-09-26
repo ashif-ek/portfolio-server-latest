896c7f5470319ab117d297b4e395b7af332571d3
Ashif e.k <159209588+ashif-ek@users.noreply.github.com>
2025-09-26 12:08:32 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,7 +16,8 @@ server.use(middlewares);
 // Use the json-server router
 server.use(router);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
+
 });
